abaf969c19ecde966d90163e561ef80bf614a084
nitadori <keigo@riken.jp>
2025-09-06 23:21:43 +0900

    C言語のまま構造体コピー版

--- check.sh
+++ check.sh
@@ -1,7 +1,10 @@
-g++-14 -O2 -Wall himenoBMTxpa.cpp -o cpp.out
-gcc-14 -O2 -Wall himenoBMTxpa.c   -o c.out
+g++-14 -O2 -Wall himenoBMTxpa.cpp    -o cpp.out
+gcc-14 -O2 -Wall himenoBMTxpa.c      -o c.out
+gcc-14 -O2 -Wall himenoBMTxpa-copy.c -o copy.out
 echo "\nC Language:"
-./c.out   S
+./c.out    L
 echo "\nC++ Language:"
-./cpp.out S
+./cpp.out  L
+echo "\nC with copy:"
+./copy.out L
 
